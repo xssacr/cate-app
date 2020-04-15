493aa7e185323fe39aa30ef79fcef071c3292eae
xiaosai <xssacr@aliyun.com>
2020-04-15 22:32:57 +0800

    mockjs 模拟 <推荐> 模块的数据

--- mock/recommend.js
+++ mock/recommend.js
@@ -1,7 +1,18 @@
 // 推荐 列表的数据接口
 
 import Mock from 'mockjs';
+let Random = Mock.Random;
 
 Mock.mock('/api/recommend/getlist', "post", options => {
-  
+  return Mock.mock({
+    "recommendlist|10": [
+      {
+        'id|+1': 1,
+        title: '@ctitle(10,30)',
+        uname: '@ctitle(4,6)',
+        lovenum: '@natural(1,1000)',
+        src: Random.image('350x' + '@natural(200,600)', '@color()', '#fff', 'png', 'hello')
+      }
+    ]
+  })
 })
--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import store from './store'
 import './plugins/vant.js'
 import './assets/styles/reset.scss';
 import './assets/styles/common.scss'
+import axios from 'axios';
+import '../mock/index';
+
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
 
